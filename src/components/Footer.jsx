ebfc9e1570989cab547cfa2c3595a91db453c015
ANKIT <ankitpaul894@gmail.com>
2023-07-18 20:31:28 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const date = new Date();
+const yr = date.getFullYear();
+
+function Footer() {
+    return <footer><p>Copyright © {yr}</p></footer>;
+}
+
+export default Footer;
